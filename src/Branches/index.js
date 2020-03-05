a50f1e005227fdf320bd28050fa5eb4acfae2205
MNikhil0210 <nikhilpavansai.machavaram@zemosolabs.com>
2020-03-05 14:53:43 +0530

    minor fixes to images, storing them in DB

--- src/Branches/index.js
+++ src/Branches/index.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import CustomDrawer from '../CustomDrawer';
 import { Toolbar } from '@material-ui/core';
-
+import CustomDrawer from '../CustomDrawer';
 
 const useStyles = makeStyles(theme => ({
     branch: {
@@ -16,76 +15,162 @@ const useStyles = makeStyles(theme => ({
         minWidth: '80px',
         maxWidth: '1000px'
     },
+    imageDistance: {
+        marginTop: '5%'
+    },
+    componentText: {
+        textAlign: 'left',
+        color: 'rgb(51,71,91)',
+        fontWeight: 400
+    },
 }));
 
 export default function Branches(props) {
+
     const classes = useStyles();
+    const pId = props.projId;
     const [branches, setBranches] = useState([]);
-    const [file, setFile] = useState([]);
-    const [layer, setLayer] = useState([]);
-    const [pid, setPid] = useState(props.projId);
+    const [files, setFiles] = useState([]);
+    const [layers, setLayers] = useState([]);
     const [temp, setTemp] = useState([]);
-    const [assets, setAssets] = useState(new Set([]));
+    const [assets, setAssets] = useState([]);
+    const [pics, setPics] = useState([]);
+    const [images, setImages] = useState([]);
+    const myMap = [];
+    var binaryList = [];
+    const [binaryString, setBinaryString] = useState([]);
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     function filterAssets(helper) {
-        setTemp(layer.filter(item => item.name.includes(helper)));
+        setTemp(layers.filter(item => item.name.includes(helper)));
     }
 
     useEffect(() => {
-        const getBranches = async () => {
-            const Abstract = require('abstract-sdk');
-            const client = new Abstract.Client({
-                accessToken: 'bb75ec9c833a43d50607d1b10ed72ae04cae4180e6eb803f228314a26a84545a'
-            });
+        fetchDataFromDB();
+        if (images.length === 0) {
+            const getItems = async () => {
 
-            const listBranch = await client.branches.list({
-                projectId: pid,
-            });
-            setBranches(listBranch);
+                const Abstract = require('abstract-sdk');
+                const client = new Abstract.Client({
+                    accessToken: 'bb75ec9c833a43d50607d1b10ed72ae04cae4180e6eb803f228314a26a84545a'
+                });
 
-            const lsFile = await client.files.list({
-                projectId: pid,
-                branchId: listBranch[0].id,
-                sha: 'latest'
-            });
-            setFile(lsFile);
+                const listBranches = await client.branches.list({
+                    projectId: pId
+                });
+                setBranches(listBranches);
 
-            //list all layers in a file
-            const lsLayer = await client.layers.list({
-                projectId: pid,
-                branchId: listBranch[0].id,
-                fileId: lsFile[0].id,
-                sha: 'latest'
-            });
-            setLayer(lsLayer);
-            const mySet = new Set([]);
-            lsLayer.forEach(layer => mySet.add(layer.name.substring(0, layer.name.indexOf("/"))))
-            setAssets(mySet)
+                const listFiles = await client.files.list({
+                    projectId: pId,
+                    branchId: listBranches[0].id
+                });
+                setFiles(listFiles);
 
-            const imageBuffer = await client.previews.raw({
-                projectId: pid,
-                branchId: listBranch[0].id,
-                fileId: lsFile[0].id,
-                layerId: "AC66241C-EE47-4585-BCA3-25D0D0B33844",
-                sha: "latest"
-            });
+                const listLayers = await client.layers.list({
+                    projectId: pId,
+                    branchId: listBranches[0].id,
+                    fileId: listFiles[0].id
+                });
+                setLayers(listLayers);
 
-            var arrayBufferView = new Uint8Array(imageBuffer);
-            var base64String = btoa(String.fromCharCode.apply(null, arrayBufferView));
-            document.getElementById('refef').src = `data:image/png;base64,${base64String}`;
+                const mySet = new Set([]);
+                listLayers.forEach(layer => {
+                    layer.name = layer.name.replace("/", "?");
+                    const startIndex = layer.name.indexOf("?");
+                    if (layer.name.indexOf("/") !== -1)
+                        mySet.add(layer.name.substring(startIndex + 1, layer.name.indexOf("/")));
+                    else {
+                        layer.name = layer.name.replace("?", "/");
+                        mySet.add(layer.name.substring(0, layer.name.indexOf("/")));
+                    }
+                })
+                setAssets(mySet);
+
+                for (var i = 0; i < listLayers.length; i++) {
+                    await sleep(300);
+                    const layer = listLayers[i]
+                    client.previews.raw({
+                        projectId: pId,
+                        branchId: listBranches[0].id,
+                        fileId: listFiles[0].id,
+                        layerId: layer.id,
+                        sha: "latest"
+                    }).then(
+                        imageBuffer => {
+                            var arrayBufferView = new Uint8Array(imageBuffer);
+                            var binary = '';
+                            var bytes = arrayBufferView
+                            var len = bytes.byteLength;
+                            for (var i = 0; i < len; i++) {
+                                binary += String.fromCharCode(bytes[i]);
+                            }
+                            binary = btoa(binary);
+                            binaryList.push(binary);
+                            console.log(binary);
+                            myMap[layer.id] = `data:image/png;base64,${binary}`;
+
+                        }
+                    )
+                }
+                setBinaryString(binaryList);
+                setPics(myMap);
+            }
+            getItems();
+            storeImagesInDB();
         }
-        getBranches();
-    }, [pid]);
+
+        else {
+            {layers.map(layer=> {images.map(image => myMap[layer.id] = `data:image/png;base64,${image}`)})}
+        }
+    }, []);
+
+    async function storeImagesInDB() {
+        fetch("http://localhost:8080/images", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                accept: "application/json"
+            },
+            body: JSON.stringify(binaryString.map(x => { return { base64String: x } }))
+        }).then(console.log('Success'));
+    }
+
+    async function fetchDataFromDB() {
+        const image_response = await fetch(
+            "http://localhost:8080/images",
+            {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+        const image_body = await image_response.json();
+        setImages(image_body);
+    }
 
     return (
         <div className={classes.branch}>
-            <CustomDrawer projects={props.showList} onChangeProjId={setPid} assets={assets} filterAssets={(x) => filterAssets(x)} />
+            <CustomDrawer assets={assets} filterAssets={(x) => filterAssets(x)} />
             <Toolbar />
             <div className={classes.components}>
-                <img className={classes.image} id="refef" alt="Component"/>
-                {temp.map(item => (
-                    <div key={item.id}>{item.name} ------ {item.id}</div>
-                ))}
+                {temp.length ? temp.map(item => (
+                    <div className={classes.imageDistance} key={item.id}>
+                        <h3>{item.name.substring(item.name.lastIndexOf("/") + 1)}</h3>
+                        <img className={classes.image} id={item.id} src={pics[item.id]} alt="Component" />
+                    </div>
+                )) :
+                    <div className={classes.componentText}>
+                        <h2>What is a component?</h2>
+                        <div style={{ marginBottom: '8%' }}>A UI element that can be used more than twice in the same way and can be broken down to its basics. It can have variants or variations (i.e. pagination with or without next arrows). It doesn’t usually have an opinion on content or task. For opinionated uses of components, see patterns.</div>
+                        <h2>Are these all of the components?</h2>
+                        <div style={{ marginBottom: '8%' }}>Nope. While just a fraction of the total system, we think it’s representative of the whole and a good introduction without losing the forest for the trees.</div>
+                        <h2>Will these components change?</h2>
+                        <div style={{ marginBottom: '8%' }}>Yup. These components are managed by a rotating group of designers and a dedicated team of front-end engineers working in partnership. This means that what designers create and what the engineers build is always up-to-date. Because designers pull from the same Sketch UI kit and because engineers pull from the same codebase, when these teams adjust or add components, those components are updated for everyone.</div>
+                    </div>}
             </div>
         </div>
     );
--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -78,7 +78,7 @@ const useStyles = makeStyles(theme => ({
         fontSize: '12px',
         fontWeight: 400
     },
-    info :{
+    info: {
         fontSize: '14px',
         color: 'rgb(51,71,91)',
         lineHeight: '20.02px',
@@ -87,8 +87,8 @@ const useStyles = makeStyles(theme => ({
         letterSpacing: '0.14994px',
     },
     componentLibText: {
-        fontSize: '24px', 
-        fontWeight: 400, 
+        fontSize: '24px',
+        fontWeight: 400,
         marginBottom: '20px',
         color: 'rgb(51,71,91)',
         lineHeight: '34.32px',
@@ -100,14 +100,14 @@ const useStyles = makeStyles(theme => ({
 export default function LandingPage() {
 
     const classes = useStyles();
-    const [page,setPage] = React.useState("MAIN");
+    const [page, setPage] = React.useState("MAIN");
     const [pId, setPid] = React.useState();
     const [projs, setProjs] = React.useState([]);
-    const [showList,setShowList] = React.useState([])
-    const [searchTerm,setSearchTerm] = React.useState("")
+    const [showList, setShowList] = React.useState([])
+    const [searchTerm, setSearchTerm] = React.useState("")
 
-    React.useEffect(()=>{
-        const getProjects = async () =>{
+    React.useEffect(() => {
+        const getProjects = async () => {
             const Abstract = require('abstract-sdk');
             const client = new Abstract.Client({
                 accessToken: 'bb75ec9c833a43d50607d1b10ed72ae04cae4180e6eb803f228314a26a84545a'
@@ -118,55 +118,293 @@ export default function LandingPage() {
             setShowList(listProjs)
         }
         getProjects();
-    },[]);
+    }, []);
 
-    React.useEffect(()=>{
-        setShowList(projs.filter(x=>x.name.toLowerCase().includes(searchTerm.toLowerCase())))
-    },[searchTerm]);
+    React.useEffect(() => {
+        setShowList(projs.filter(x => x.name.toLowerCase().includes(searchTerm.toLowerCase())))
+    }, [searchTerm]);
 
-    if(page==="BRANCH"){
-        return(
+    if (page === "BRANCH") {
+        return (
             <div>
-                <CustomAppBar onSearch = {searchTerm}/>
-                <Branches showList={projs} projId={pId}/>
+                <CustomAppBar onSearch={searchTerm} />
+                <Branches showList={projs} projId={pId} />
             </div>
         );
     }
     else
         return (
-            <div style={{background: '#fafafa'}}>
-            <CustomAppBar onSearch = {setSearchTerm}/>
-            <div className={classes.midColor}>
-                <div className={classes.midText}>
-                    <Typography className={classes.zemTechnology}>
-                        ZeMoSo Technologies
+            <div style={{ background: '#fafafa' }}>
+                <CustomAppBar onSearch={setSearchTerm} />
+                <div className={classes.midColor}>
+                    <div className={classes.midText}>
+                        <Typography className={classes.zemTechnology}>
+                            ZeMoSo Technologies
                     </Typography>
-                    <Typography className={classes.componentLib}>
-                        Component Library
+                        <Typography className={classes.componentLib}>
+                            Component Library
                     </Typography>
+                    </div>
                 </div>
-            </div>
-            <div className={classes.body}>
-                <div className={classes.bodyFont}>Welcome to Component Library</div>
-                <div className={classes.info}>
-                    <div>HubSpot Canvas is the design system that we at HubSpot use to build our products. This library showcases the building blocks that make up our design system, from colors and typography to React-based components and data visualization tools. What you see here is a subset of our components and styles, pulled straight from our production code.</div>
-                    <br/>
-                    <div>This library is a window into how we build our products here at HubSpot and what it’s like to build the HubSpot product. We’re sharing it because we’re proud of the time and effort we’ve put into creating our design system and optimizing it for developers and designers so that we can keep it evergreen.</div>
-                    <br/>
-                    <div>If you’re an engineer or designer and you’re excited by what you see here, get in touch.</div>
+                <div className={classes.body}>
+                    <div className={classes.bodyFont}>Welcome to Component Library</div>
+                    <div className={classes.info}>
+                        <div>HubSpot Canvas is the design system that we at HubSpot use to build our products. This library showcases the building blocks that make up our design system, from colors and typography to React-based components and data visualization tools. What you see here is a subset of our components and styles, pulled straight from our production code.</div>
+                        <br />
+                        <div>This library is a window into how we build our products here at HubSpot and what it’s like to build the HubSpot product. We’re sharing it because we’re proud of the time and effort we’ve put into creating our design system and optimizing it for developers and designers so that we can keep it evergreen.</div>
+                        <br />
+                        <div>If you’re an engineer or designer and you’re excited by what you see here, get in touch.</div>
+                    </div>
                 </div>
-            </div>
-            <div className={classes.body}>
-                <div className={classes.componentLibText}>Explore Zorro Component Library:</div>
-            </div>
-            
-            <CustomCard projects={showList} respond={(x)=>{setPid(x); setPage("BRANCH")}} />
-            
+                <div className={classes.body}>
+                    <div className={classes.componentLibText}>Explore Zorro Component Library:</div>
+                </div>
 
-            <div className={classes.footer}>
-                <div className={classes.footerLine}></div>
-                <div className={classes.copyright}>Copyright © 2018 – 2020 Zemoso Technologies Pvt Lmt, Inc.</div>
+                <CustomCard projects={showList} respond={(x) => { setPid(x); setPage("BRANCH") }} />
+
+
+                <div className={classes.footer}>
+                    <div className={classes.footerLine}></div>
+                    <div className={classes.copyright}>Copyright © 2018 – 2020 Zemoso Technologies Pvt Lmt, Inc.</div>
+                </div>
             </div>
-        </div>
-    );
+        );
 }
+
+// return (
+//     <div className={classes.branch}>
+//         <CustomDrawer assets={assets} filterAssets={(x) => filterAssets(x)} />
+//         <Toolbar />
+//         <div className={classes.components}>
+//             {temp.length ? temp.map(item => (
+//                 <div className={classes.imageDistance} key={item.id}>
+//                     <h3>{item.name.substring(item.name.lastIndexOf("/") + 1)}</h3>
+//                     <img className={classes.image} id={item.id} src={pics[item.id]} alt="Component" />
+//                 </div>
+//             )) :
+//                 <div className={classes.componentText}>
+//                     <h2>What is a component?</h2>
+//                     <div style={{ marginBottom: '8%' }}>A UI element that can be used more than twice in the same way and can be broken down to its basics. It can have variants or variations (i.e. pagination with or without next arrows). It doesn’t usually have an opinion on content or task. For opinionated uses of components, see patterns.</div>
+//                     <h2>Are these all of the components?</h2>
+//                     <div style={{ marginBottom: '8%' }}>Nope. While just a fraction of the total system, we think it’s representative of the whole and a good introduction without losing the forest for the trees.</div>
+//                     <h2>Will these components change?</h2>
+//                     <div style={{ marginBottom: '8%' }}>Yup. These components are managed by a rotating group of designers and a dedicated team of front-end engineers working in partnership. This means that what designers create and what the engineers build is always up-to-date. Because designers pull from the same Sketch UI kit and because engineers pull from the same codebase, when these teams adjust or add components, those components are updated for everyone.</div>
+//                 </div>}
+//         </div>
+//     </div>
+// );
+// function filterAssets(helper) {
+//     setTemp(layer.filter(item => item.name.includes(helper)));
+// }
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
+// useEffect(() => {
+//     async function fetchData() {
+//         const image_response = await fetch(
+//             "http://localhost:8080/images",
+//             {
+//                 headers: {
+//                     "Accept": "application/json",
+//                     "Content-Type": "application/json"
+//                 }
+//             }
+//         );
+//         const image_body = await image_response.json();
+//         setImages(image_body);
+//     }
+//     fetchData();
+// }, []);
+// async function makePostRequest() {
+//     fetch("http://localhost:8080/images", {
+//         method: "POST",
+//         headers: {
+//             "content-type": "application/json",
+//             accept: "application/json"
+//         },
+//         body: JSON.stringify(binaryImagesList.map(x => { return { base64String: x } }))
+//     });
+// }
+// function showImages(item) {
+//     for (var i = 0; i < lsLayer.length; i++) {
+//         const layer = lsLayer[i]
+//         myMap[layer.id] = `data:image/png;base64,${item}`;
+//     }
+// }
+
+// import React, { useState, useEffect } from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import CustomDrawer from '../CustomDrawer';
+// import { Toolbar } from '@material-ui/core';
+
+
+// const useStyles = makeStyles(theme => ({
+//     branch: {
+//         marginLeft: '25%'
+//     },
+//     components: {
+//         textAlign: 'center',
+//     },
+//     image: {
+//         minHeight: '80px',
+//         minWidth: '80px',
+//         maxWidth: '1000px'
+//     },
+//     imageDistance: {
+//         marginTop: '5%'
+//     },
+//     componentText: {
+//         textAlign: 'left',
+//         color: 'rgb(51,71,91)',
+//         fontWeight: 400
+//     },
+// }));
+
+// export default function Branches(props) {
+
+//     const classes = useStyles();
+//     const pid = props.projId;
+//     const [branch, setBranch] = useState([]);
+//     const [file, setFile] = useState([]);
+//     const [layer, setLayer] = useState([]);
+//     const [images, setImages] = useState([]);
+//     const [filteredComponent, setFilteredComponent] = useState([]);
+
+//     function filterAssets(helper) {
+//         setFilteredComponent(layer.filter(item => item.name.includes(helper)));
+//     }
+
+//     useEffect(() => {
+//         async function fetchDataFromDB() {
+//             const image_response = await fetch(
+//                 "http://localhost:8080/images",
+//                 {
+//                     headers: {
+//                         "Accept": "application/json",
+//                         "Content-Type": "application/json"
+//                     }
+//                 }
+//             );
+//             const image_body = await image_response.json();
+//             setImages(image_body);
+//         }
+//         fetchDataFromDB();
+//     }, []);
+
+//     if (images.length === 0) {
+//         getItems();
+//         //makePostRequest();
+//     }
+
+//     // async function makePostRequest() {
+//     //     fetch("http://localhost:8080/images", {
+//     //         method: "POST",
+//     //         headers: {
+//     //             "content-type": "application/json",
+//     //             accept: "application/json"
+//     //         },
+//     //         body: JSON.stringify(binaryImagesList.map(x => { return { base64String: x } }))
+//     //     });
+//     // }
+
+//     async function getItems() {
+//         const Abstract = require('abstract-sdk');
+//         const client = new Abstract.Client({
+//             accessToken: 'bb75ec9c833a43d50607d1b10ed72ae04cae4180e6eb803f228314a26a84545a'
+//         });
+
+//         const listBranch = await client.branches.list({
+//             projectId: pid
+//         });
+//         setBranch(listBranch);
+
+//         const listFile = await client.files.list({
+//             projectId: pid,
+//             branchId: listBranch[0].id
+//         });
+//         setFile(listFile);
+
+//         const listLayer = await client.layers.list({
+//             projectId: pid,
+//             branchId: listBranch[0].id,
+//             fileId: listFile[0].id
+//         });
+//         setLayer(listLayer);
+
+//         for(var i=0;i<listLayer.length;i++){        
+//             const imageBuffer = await client.previews.raw({
+//                 projectId: pid,
+//                 branchId: listBranch[0].id,
+//                 fileId: listFile[0].id,
+//                 layerId: listLayer[i].id,
+//                 sha: "latest"
+//             });
+
+//             var arrayBufferView = new Uint8Array(imageBuffer);
+//             var base64String = btoa(String.fromCharCode.apply(null, arrayBufferView));
+//             console.log(base64String);
+//         }
+//     }
+
+//     function logconsole() {
+//         console.log(branch);
+//         console.log(file);
+//         console.log(layer);
+//     }
+
+//     return (
+//         <div className={classes.branch}>
+//             <Toolbar />
+//             <Toolbar />
+//             <button onClick={logconsole}>Click me for console</button>
+//             {/* <CustomDrawer assets={assets} filterAssets={(x) => filterAssets(x)} />
+//             <Toolbar />
+//             <div className={classes.components}>
+//                 {temp.length ? temp.map(item => (
+//                     <div className={classes.imageDistance} key={item.id}>
+//                         <h3>{item.name.substring(item.name.lastIndexOf("/") + 1)}</h3>
+//                         <img className={classes.image} id={item.id} src={pics[item.id]} alt="Component" />
+//                     </div>
+//                 )) :
+//                     <div className={classes.componentText}>
+//                         <h2>What is a component?</h2>
+//                         <div style={{ marginBottom: '8%' }}>A UI element that can be used more than twice in the same way and can be broken down to its basics. It can have variants or variations (i.e. pagination with or without next arrows). It doesn’t usually have an opinion on content or task. For opinionated uses of components, see patterns.</div>
+//                         <h2>Are these all of the components?</h2>
+//                         <div style={{ marginBottom: '8%' }}>Nope. While just a fraction of the total system, we think it’s representative of the whole and a good introduction without losing the forest for the trees.</div>
+//                         <h2>Will these components change?</h2>
+//                         <div style={{ marginBottom: '8%' }}>Yup. These components are managed by a rotating group of designers and a dedicated team of front-end engineers working in partnership. This means that what designers create and what the engineers build is always up-to-date. Because designers pull from the same Sketch UI kit and because engineers pull from the same codebase, when these teams adjust or add components, those components are updated for everyone.</div>
+//                     </div>}
+//             </div> */}
+//         </div>
+//     );
+
+// }
+
+// for (var i = 0; i < lsLayer.length; i++) {
+//     await sleep(300);
+//     const layer = lsLayer[i]
+//     client.previews.raw({
+//         projectId: pid,
+//         branchId: listBranch[0].id,
+//         fileId: lsFile[0].id,
+//         layerId: layer.id,
+//         sha: "latest"
+//     }).then(
+//         imageBuffer => {
+//             var arrayBufferView = new Uint8Array(imageBuffer);
+//             var binary = '';
+//             var bytes = arrayBufferView
+//             var len = bytes.byteLength;
+//             for (var i = 0; i < len; i++) {
+//                 binary += String.fromCharCode(bytes[i]);
+//             }
+//             binary = btoa(binary);
+//             binaryList.push(binary);
+//             console.log(binary);
+//             myMap[layer.id] = `data:image/png;base64,${binary}`
+
+//         }
+//     )
+// }
+// setBinaryImagesList(binaryList);
+// setPics(myMap);
