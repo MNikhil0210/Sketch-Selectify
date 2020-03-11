4176ba66e5b5477b8171fe42177cd068a2992d4a
MNikhil0210 <nikhilpavansai.machavaram@zemosolabs.com>
2020-03-11 11:25:41 +0530

    Updates

---
+++ precache-manifest.3bd5009b4be1a9d85874b78c701bf4b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3df290630eba504787f02302c3ec31",
+    "url": "/Sketch-Selectify/index.html"
+  },
+  {
+    "revision": "4e297ac9d92baaa02af2",
+    "url": "/Sketch-Selectify/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e5d089e9e50661f9fcf6",
+    "url": "/Sketch-Selectify/static/js/2.86516ca4.chunk.js"
+  },
+  {
+    "revision": "aa72db0bfab74e637ca50275ccc280a2",
+    "url": "/Sketch-Selectify/static/js/2.86516ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e297ac9d92baaa02af2",
+    "url": "/Sketch-Selectify/static/js/main.2ffee43f.chunk.js"
+  },
+  {
+    "revision": "db2bf4724fa21b55bb79",
+    "url": "/Sketch-Selectify/static/js/runtime-main.4e2ff7c1.js"
+  },
+  {
+    "revision": "7fa4b58bdd168b1f6013d5c9f9f0b776",
+    "url": "/Sketch-Selectify/static/media/brush.7fa4b58b.svg"
+  },
+  {
+    "revision": "fb325b82dc44336ce8a77a0f3ce912ac",
+    "url": "/Sketch-Selectify/static/media/dove.fb325b82.svg"
+  },
+  {
+    "revision": "e56342e57c91bceda9fe3269c4107d08",
+    "url": "/Sketch-Selectify/static/media/pencil.e56342e5.svg"
+  },
+  {
+    "revision": "b1fb1d7a7bc9387368666386b88ed3da",
+    "url": "/Sketch-Selectify/static/media/zemoso.b1fb1d7a.svg"
+  }
+]);
